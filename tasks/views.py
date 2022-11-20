a2e2ee684e00b23d91a62db9de61b1dddec6b63c
Fadiil <fadilthoriq201@gmail.com>
2022-11-20 13:34:30 +0700

    crud done

--- tasks/views.py
+++ tasks/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 
 from .models import *
 from .forms import *
-# Create your views here.
 
 def index(request):
     tasks = Task.objects.all()
@@ -18,3 +17,26 @@ def index(request):
     
     context = {'tasks':tasks, 'form':form}
     return render(request, 'tasks/list.html', context)
+
+def update(request, primary_key):
+    task = Task.objects.get(id=primary_key)
+    form = TaskForm(instance=task)
+    
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+    
+    context = {'form':form}
+    return render(request, 'tasks/update.html', context)
+
+def deleteTask(request, primary_key):
+	item = Task.objects.get(id=primary_key)
+
+	if request.method == 'POST':
+		item.delete()
+		return redirect('/')
+
+	context = {'item':item}
+	return render(request, 'tasks/delete.html', context)
